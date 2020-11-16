4ff47dc4d6a4756d55fd40d50328bdc9bca10b01
Stephenalexngo <51846517+Stephenalexngo@users.noreply.github.com>
2020-11-16 21:06:26 +0800

    Initial Readfile and DFA added and text file

--- PS-Scanner/src/Main.java
+++ PS-Scanner/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        File file = new File("inputfile.txt");
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+
+        while( (line = br.readLine()) != null){
+            String[] arr = line.replace(",", "").split(" ");
+        
+            for (String word : arr){
+                if(!word.equals("")){
+                    
+                }
+            }
+        }
+
+        fr.close();
     }
 }
